adebeaed1c86a714e41a1317f1140f507f9ea03e
Danke1732 <s0lfa1732@gmail.com>
2021-07-24 17:24:40 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+export default function App() {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
